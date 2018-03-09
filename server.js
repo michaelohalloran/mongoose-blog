bb48111fd1acffeeb47ae87c96ed354aeb8d6314
MJ <kipsang20323@gmail.com>
2018-03-09 00:37:25 -0700

    Add final changes to routes

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ app.use(jsonParser);
 // const path = require('path');
 
 // app.get('/blog-posts', (req, res) => {
+    //Express method, path comes from Node, allows us to join folder names to a path, path.join uses
+    //global var __dirname (current directory where file is executing, wherever folder we put this is located (C://Mike etc.))
+    //this mashes these 2 things together to make one string
 //     res.sendFile(path.join(__dirname, '/index.html'))
+    // let fullPath = __dirname + "/index.html" > this = C:\...\mongoose_blog/index.html
 // });
 
 app.get('/blog-posts', (req,res) => {
@@ -27,12 +31,15 @@ app.get('/blog-posts', (req,res) => {
     //return only 10
     .limit(10)
     //this promise returns an array or object of blogPost objects?
-    .then(blogPost=> {
-        res.json({
-        title: blogPost.title,
-        content: blogPost.content,
-        author: blogPost.authorName
-        });
+    .then(blogPosts=> {
+        // console.log(blogPost);
+        //json method wraps this string with type json and sends it on response channel as a string
+        //changes this from JSON into string (notifies browser it's a JSON type so it can be parsed)
+        //JSON is used to refer to type JSON, but it's also a browser library, so JSON.stringify is 
+        //method on this library, changes JSON object into string
+        res.json(blogPosts);
+            //blogPost is model object based on blogPost schema/class, this here is an instance, we 
+            //are creating a JSON object which will be serialized 
     })
     .catch(err=>{
         console.error(err);
@@ -58,11 +65,12 @@ app.get('/blog-posts/:id', (req,res)=>{
     });
 });
 
-app.post('/blog-posts', (req, res)=> {
 
+app.post('/blog-posts', (req, res)=> {
     //this checks if all fields are included
     const requiredFields = ['title', 'content', 'author'];
     for(let i = 0; i < requiredFields.length; i++) {
+        let field = requiredFields[i];
         if(!(field in req.body)) {
             const message = `Missing field \`${field}\` in request body`;
             console.error(message);
@@ -70,6 +78,9 @@ app.post('/blog-posts', (req, res)=> {
         }
     }
 
+    //Mongoose's create method, this checks that we're not passing e.g. a number for the title, since this would
+    //violate the schema (which requires a string)
+    //actual user would be passing req.body via a form, though we are doing it in Postman
     blogPost.create({
         title: req.body.title,
         content: req.body.content,
@@ -83,23 +94,43 @@ app.post('/blog-posts', (req, res)=> {
 });
 
 app.put('/blog-posts/:id', (req, res)=>{
-
-    blogPost.update({
-        _id: req.params.id,
-        title: req.body.title,
-        content: req.body.content,
-        author: req.body.author
-    })
-
+    
     if(req.params.id !== req.body.id) {
         const message = `Request id ${req.params.id} and request body id ${req.body.id} do not match`;
         res.status(400).send(message);
     }
-    res.status(200).json(//updatedObj)
+
+    const updatedPost = {};
+
+    const updateFields = ['title', 'content', 'author'];
+    for(let i = 0; i < updateFields.length; i++) {
+        if(updateFields[i] in req.body) {
+            updatedPost[updateFields[i]] = req.body[updateFields[i]];
+        }
+    }
+
+    blogPost.findByIdAndUpdate(req.params.id, {$set: updatedPost}, {new:true})
+        .then(updatedPost => res.status(204).end())
+        .catch(err=>res.status(500).json({message: "Error"}));
+
+    // let post = blogPost.update({
+    //     _id: req.params.id,
+    //     title: req.body.title,
+    //     content: req.body.content,
+    //     author: req.body.author
+    // })
+    // .then(updatedPost => res.status(204).end()) 
+    // .catch(err=>res.status(500).json({message: "Error thrown"}));
+
+    
+    // res.status(200).json(post);
 });
 
 app.delete('/blog-posts/:id', (req, res)=>{
     blogPost.findByIdAndRemove(req.params.id)
+    .then(()=> {
+        res.status(204).json({message: "success"});
+    })
     .catch(err=>{
         if(err) {
             console.error(err);
@@ -133,6 +164,7 @@ function runServer() {
 }
 
 function closeServer() {
+    //disconnects this Node script from Mongo DB, now we can't use Mongoose anymore
     return mongoose.disconnect().then(()=>{
         return new Promise((resolve, reject)=>{
             console.log('Closing server');
