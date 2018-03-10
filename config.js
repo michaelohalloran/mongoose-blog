b752fef34a47e858a955c93c7856c57cc0ab3036
MJ <kipsang20323@gmail.com>
2018-03-09 17:18:17 -0700

    Change config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/tempTestDb';
+// exports.DATABASE_URL = 'mongodb://mongooser23:resoog3245@ds263408.mlab.com:63408/mongoose-blog';
 exports.PORT = process.env.PORT || 8080;
 
